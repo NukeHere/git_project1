2e0039256599456a51a900efe9b74f1614a6dd47
Яндекс <1@mail.ru>
2023-01-18 16:52:52 +0300

    Запускаете MainGame(width_screen, height_screen)

--- game_PvP/main_game.py
+++ game_PvP/main_game.py
@@ -54,68 +54,75 @@ class BaseTank(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=self.rect.center)
 
 
+class MainGame:
+    def __init__(self, w, h):
+        global width, height, all_sprites, player
+        pygame.init()
+        size = width, height = w, h
+        screen = pygame.display.set_mode(size)
+        all_sprites = pygame.sprite.Group()
+        clock = pygame.time.Clock()
+        tank1 = BaseTank(100, 100, 0, 'tank_body.png')
+        tank2 = BaseTank(300, 300, 0, 'nlo.jpg')
+        camera = Camera()
+        running = True
+        player = tank1
+        m1 = False
+        m2 = False
+        maxspeed = 8
+        k2 = 1
+        k3 = 1
+        while running:
+            k = maxspeed / abs(player.crspeed if player.crspeed != 0 else 1)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        m1 = True
+                        k2 = 1
+                    if event.key == pygame.K_s:
+                        m1 = True
+                        k2 = -0.5
+                    if event.key == pygame.K_a:
+                        m2 = True
+                        k3 = -1
+                    if event.key == pygame.K_d:
+                        m2 = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        m1 = False
+                    if event.key == pygame.K_s:
+                        m1 = False
+                        k2 = 1
+                    if event.key == pygame.K_a:
+                        m2 = False
+                        k3 = 1
+                    if event.key == pygame.K_d:
+                        m2 = False
+            if m1 and abs(player.crspeed) < maxspeed:
+                player.crspeed += 0.002 * k * k2
+            else:
+                if abs(player.crspeed) < 1:
+                    player.crspeed = 0
+                elif player.crspeed > 0:
+                    player.crspeed -= 0.01
+                elif player.crspeed < 0:
+                    player.crspeed += 0.01
+            if m2:
+                player.ang += (1 * k3 * 0.017)
+                player.ang = (player.ang / 0.017) % 360 * 0.017
+            # print(player.crspeed, k, player.ang / 0.017, player.crspeed * math.cos(player.ang),
+            #                                            player.crspeed * math.sin(player.ang)) debug out
+            screen.fill((0, 0, 0))
+            camera.update(player)
+            for sprite in all_sprites:
+                camera.apply(sprite)
+            all_sprites.update()
+            all_sprites.draw(screen)
+            pygame.display.flip()
+            clock.tick(100)
+
+
 if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
-    all_sprites = pygame.sprite.Group()
-    clock = pygame.time.Clock()
-    tank1 = BaseTank(100, 100, 0, 'tank_body.png')
-    tank2 = BaseTank(300, 300, 0, 'nlo.jpg')
-    camera = Camera()
-    running = True
-    player = tank1
-    m1 = False
-    m2 = False
-    maxspeed = 8
-    k2 = 1
-    k3 = 1
-    while running:
-        k = maxspeed / abs(player.crspeed if player.crspeed != 0 else 1)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    m1 = True
-                    k2 = 1
-                if event.key == pygame.K_s:
-                    m1 = True
-                    k2 = -0.5
-                if event.key == pygame.K_a:
-                    m2 = True
-                    k3 = -1
-                if event.key == pygame.K_d:
-                    m2 = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                    m1 = False
-                if event.key == pygame.K_s:
-                    m1 = False
-                    k2 = 1
-                if event.key == pygame.K_a:
-                    m2 = False
-                    k3 = 1
-                if event.key == pygame.K_d:
-                    m2 = False
-        if m1 and abs(player.crspeed) < maxspeed:
-            player.crspeed += 0.002 * k * k2
-        else:
-            if abs(player.crspeed) < 1:
-                player.crspeed = 0
-            elif player.crspeed > 0:
-                player.crspeed -= 0.25
-            elif player.crspeed < 0:
-                player.crspeed += 0.25
-        if m2:
-            player.ang += (1 * k3 * 0.017)
-            player.ang = (player.ang / 0.017) % 360 * 0.017
-        print(player.crspeed, k, player.ang / 0.017, 1 * k3 * 0.017)
-        screen.fill((0, 0, 0))
-        camera.update(player)
-        for sprite in all_sprites:
-            camera.apply(sprite)
-        all_sprites.update()
-        all_sprites.draw(screen)
-        pygame.display.flip()
-        clock.tick(100)
+    a = MainGame(800, 600)
