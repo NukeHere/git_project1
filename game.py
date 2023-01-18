aa87bf6836d9a94df79e9b6d7f51203df522d78b
skrandark <85711800+skrandark@users.noreply.github.com>
2023-01-18 16:07:40 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,44 @@
+import pygame
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                    self.cell_size), 1)
+
+
+def main():
+    pygame.init()
+    size = 800, 800
+    screen = pygame.display.set_mode(size)
+    board = Board(4, 4)
+    board.set_view(50, 50, 55)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        board.render(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
