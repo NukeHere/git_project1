6c65d2f1cb3c8b25b5d98bdf4461b23892d45973
Kvasif <86017752+Kvasif@users.noreply.github.com>
2023-01-25 17:15:28 +0300

    Add files via upload

--- Menu/viborK.py
+++ Menu/viborK.py
@@ -7,17 +7,21 @@ class vibor_kart(GlavnoeMenu):
         pygame.font.init()
         self.screen.fill((0, 0, 0))
         self.screen.blit(pic, (0, 0))
-        pygame.draw.rect(self.screen, ('#808080'), (300, 225, 200, 65))
-        pygame.draw.rect(self.screen, ('#808080'), (300, 310, 200, 65))
-        pygame.draw.rect(self.screen, ('#808080'), (10, 535, 150, 50))
-        pygame.draw.rect(self.screen, ('#808080'), (300, 395, 200, 65))
-        q = pygame.font.SysFont('arial', 25)
+        pygame.draw.rect(self.screen, ('#808080'), (300 * self.screen_resolution, 225 * self.screen_resolution,
+                                                    200 * self.screen_resolution, 65 * self.screen_resolution))
+        pygame.draw.rect(self.screen, ('#808080'), (300 * self.screen_resolution, 310 * self.screen_resolution,
+                                                    200 * self.screen_resolution, 65 * self.screen_resolution))
+        pygame.draw.rect(self.screen, ('#808080'), (10 * self.screen_resolution, 535 * self.screen_resolution,
+                                                    150 * self.screen_resolution, 50 * self.screen_resolution))
+        pygame.draw.rect(self.screen, ('#808080'), (300 * self.screen_resolution, 395 * self.screen_resolution,
+                                                    200 * self.screen_resolution, 65 * self.screen_resolution))
+        q = pygame.font.SysFont('arial', int(25) * self.screen_resolution)
         txt = q.render('Лес', True, (255, 255, 255))
-        self.screen.blit(txt, (365, 245))
+        self.screen.blit(txt, (365 * self.screen_resolution, 245 * self.screen_resolution))
         txt = q.render('Болото', True, (255, 255, 255))
-        self.screen.blit(txt, (365, 330))
+        self.screen.blit(txt, (365 * self.screen_resolution, 330 * self.screen_resolution))
         txt = q.render('Пустыня', True, (255, 255, 255))
-        self.screen.blit(txt, (365, 415))
+        self.screen.blit(txt, (365 * self.screen_resolution, 415 * self.screen_resolution))
         txt = q.render('Назад', True, (255, 255, 255))
         self.screen.blit(txt, (50, 545))
         return list([[300, 225, 300 + 200, 225 + 65], [300, 310, 300 + 200, 310 + 65],
